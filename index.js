771ac33fa48ece7a979fb742a531da010c3fa703
Sara <saragonzalezburgin@hotmail.com>
2020-02-04 17:26:21 +0100

    Create random color generator

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomColor = require('randomcolor');
+
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+console.log(color);
+
+const chalk = require('chalk');
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('#######                 #######'));
+console.log(chalk.hex(color).bold(`#######    ${color}      #######`));
+console.log(chalk.hex(color).bold('#######                 #######'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
+console.log(chalk.hex(color).bold('###############################'));
